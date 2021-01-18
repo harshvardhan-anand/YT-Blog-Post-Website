939d9871f00be2b0c11dc38fa16ad98d88fe575b
Harshvardhan Anand <50915996+harshvardhan-anand@users.noreply.github.com>
2021-01-18 23:25:18 +0530

    Models

--- BlogPost/myapp/models.py
+++ BlogPost/myapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+
+    choice = (
+        ('PUBLISHED', 'published'),
+        ('DRAFT', 'draft')
+    )
+
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(blank=True)
+    body = models.TextField()
+    published = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(choices = choice, max_length=50)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
