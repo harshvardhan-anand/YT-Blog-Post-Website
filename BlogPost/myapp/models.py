2a5128b8d1411c41e22b5baee0aafacf02fdfb62
Harshvardhan Anand <50915996+harshvardhan-anand@users.noreply.github.com>
2021-02-05 20:26:07 +0530

    Custom Manager

--- BlogPost/myapp/models.py
+++ BlogPost/myapp/models.py
@@ -3,6 +3,11 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status='published')
+
 class Post(models.Model):
 
     choice = (
@@ -18,6 +23,8 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(choices = choice, max_length=50)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    objects = models.Manager()
+    new_manager = PublishedManager()
 
     def __str__(self):
         return self.title
