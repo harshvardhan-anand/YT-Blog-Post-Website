a86b71391897485e7d1e19ba1ff519248f506e68
Harshvardhan Anand <50915996+harshvardhan-anand@users.noreply.github.com>
2021-02-18 20:20:11 +0530

    List View

--- BlogPost/myapp/views.py
+++ BlogPost/myapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
-def homepage(request):
-    return render(request, 'myapp/homepage.html')
+def post_list(request):
+    all_posts = Post.objects.all()
+    return render(request, "myapp/homepage.html", context={'posts':all_posts})
