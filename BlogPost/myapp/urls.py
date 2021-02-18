e343422ea659a9f09914a6058c163d845bbc620b
Harshvardhan Anand <50915996+harshvardhan-anand@users.noreply.github.com>
2021-02-18 20:39:12 +0530

    Detail View

--- BlogPost/myapp/urls.py
+++ BlogPost/myapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'myapp'
+
 urlpatterns = [
-    path("list/", views.post_list, name='listview')
+    path("list/", views.post_list, name='listview'),
+    path("detail/<int:pk>", views.post_detail, name='detailview')
 ]
--- BlogPost/myapp/views.py
+++ BlogPost/myapp/views.py
@@ -6,3 +6,7 @@ from .models import Post
 def post_list(request):
     all_posts = Post.objects.all()
     return render(request, "myapp/homepage.html", context={'posts':all_posts})
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk = pk)
+    return render(request, 'myapp/detail.html', {'post':post})
