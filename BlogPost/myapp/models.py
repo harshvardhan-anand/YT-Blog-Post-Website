055b535ff6b83094d2c08880b90be85595e6826a
Harshvardhan Anand <50915996+harshvardhan-anand@users.noreply.github.com>
2021-04-20 22:51:14 +0530

    canonical url

--- BlogPost/myapp/models.py
+++ BlogPost/myapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 
@@ -28,3 +29,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self, ):
+        return reverse('myapp:detailview', args=[self.pk])
