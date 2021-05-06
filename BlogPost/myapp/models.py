3f9eed7f41b58ca6ce881c525c2f8170250a27a3
Harshvardhan Anand <50915996+harshvardhan-anand@users.noreply.github.com>
2021-05-06 14:51:27 +0530

    Adding Tags - Taggit

--- BlogPost/myapp/models.py
+++ BlogPost/myapp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 # Create your models here.
 
@@ -26,6 +27,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     objects = models.Manager()
     new_manager = PublishedManager()
+    tags = TaggableManager()
 
     def __str__(self):
         return self.title
