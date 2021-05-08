2ca4138c76ffe12932ebe40554638465d5f7b78f
Harshvardhan Anand <50915996+harshvardhan-anand@users.noreply.github.com>
2021-05-08 18:02:25 +0530

    Inclusion Tag

--- BlogPost/myapp/templatetags/blog_tags.py
+++ BlogPost/myapp/templatetags/blog_tags.py
@@ -6,3 +6,11 @@ register = template.Library()
 @register.simple_tag
 def total_post():
     return Post.objects.count()
+
+
+@register.inclusion_tag('myapp/show_latest_post.html')
+def show_latest_posts(count=3):
+    latest_posts = Post.objects.filter(status='published').order_by('-created')[:count]
+    return {
+        'latest_posts':latest_posts
+    }
